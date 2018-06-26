fd26f112710819510429b31a397b71582568921b
John Vollmer <vollme22@uwm.edu>
2018-06-26 07:54:03 -0500

    merging

---
+++ HumaneSocietyStarter/HumaneSociety/Query.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public static class Query
+    {
+        public static void RunEmployeeQueries(Employee employee, string str)
+        {
+
+        }
+        public static void GetPendingAdoptions()
+        {
+
+        }
+        public static void UpdateAdoption(bool genericBoolean, ClientAnimalJunction clientAnimalJunction)
+        {
+
+        }
+        public static void GetShots(Animal animal)
+        {
+
+        }
+        public static void UpdateShot(String str, Animal animal)
+        {
+
+        }
+        public static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
+        {
+
+        }
+        public static void RemoveAnimal(Animal animal)
+        {
+
+        }
+        public static void AddAnimal(Animal animal)
+        {
+
+        }
+        public static int GetBreed()
+        {
+            return 0;
+        }
+        public static int GetDiet()
+        {
+            return 0;
+        }
+        public static int GetLocation()
+        {
+            return 0;
+        }
+        public static void EmployeeLogin(string userName, string password)
+        {
+
+        }
+        public static void RetrieveEmployeeUser(string email, int employeeNumber)
+        {
+
+        }
+        public static void AddUsernameAndPassword(Employee employee)
+        {
+
+        }
+        public static bool CheckEmployeeUserNameExist(string username)
+        {
+            return false;
+        }
+        public static Client GetClient(string userName, string password)
+        {
+            Client client = new Client();
+            return client;
+        }
+        public static void GetUserAdoptionStatus(Client client)
+        {
+
+        }
+        public static void GetAnimalByID(int iD)
+        {
+
+        }
+        public static void Adopt(Animal animal, Client client)
+        {
+
+        }
+        public static void RetrieveClients()
+        {
+
+        }
+        public static void GetStates()
+        {
+
+        }
+        public static void AddNewClient(string firstName, string lastName, string username, string password, string email, string streetAddress, int zipCode, int state)
+        {
+
+        }
+        public static void updateClient(Client client)
+        {
+
+        }
+        public static void UpdateUsername(Client client)
+        {
+
+        }
+        public static void UpdateEmail(Client client)
+        {
+
+        }
+        public static void UpdateAddress(Client client)
+        {
+
+        }
+        public static void UpdateFirstName(Client client)
+        {
+
+        }
+        public static void UpdateLastName(Client client)
+        {
+
+        }
+    }
+}
