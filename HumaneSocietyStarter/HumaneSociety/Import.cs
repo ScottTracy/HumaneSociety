cf50818280a0926f5a25db8c94ce8ba24d3d0a9a
Scott Tracy <scott.tracy.st@gmail.com>
2018-06-28 10:18:31 -0500

    added gitignore file and told git to stop tracking

---
+++ HumaneSocietyStarter/HumaneSociety/Import.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public class Import
+    {
+        public  void ImportCSV()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:/projects/HumaneSocietyStarter/HumaneSocietyStarter/HumaneSocietyAnimals/HumaneSocietyAnimals/Animals.csv");
+            Console.WriteLine(lines);
+
+        }
+    }
+}
--- HumaneSocietyStarter/HumaneSociety/Program.cs
+++ HumaneSocietyStarter/HumaneSociety/Program.cs
@@ -10,13 +10,15 @@ namespace HumaneSociety
     {
         static void Main(string[] args)
         {
-            Animal[] animals = new Animal[]
-            {
-                new Animal {ID = 1001, name = "Murdock", breed = null, weight = 15, age = 2, diet = null, location = null, demeanor = "skittish", kidFriendly = true, petFriendly = false, gender = null, adoptionStatus = "adopted", Employee_ID = null },
-                new Animal {ID = 1002, name = "Loki", breed = null, weight = 18, age = 3, diet = null, location = null, demeanor = "cuddly", kidFriendly = true, petFriendly = true, gender = null, adoptionStatus = "adopted", Employee_ID = null },
-                new Animal {ID = 1003, name = "Rowdy", breed = null, weight = 20, age = 8, diet = null, location = null, demeanor = "deceased", kidFriendly = true, petFriendly = true, gender = null, adoptionStatus = "not adopted", Employee_ID = null },
-            };
-            PointOfEntry.Run();
+            //Animal[] animals = new Animal[]
+            //{
+            //    new Animal {ID = 1001, name = "Murdock", breed = null, weight = 15, age = 2, diet = null, location = null, demeanor = "skittish", kidFriendly = true, petFriendly = false, gender = null, adoptionStatus = "adopted", Employee_ID = null },
+            //    new Animal {ID = 1002, name = "Loki", breed = null, weight = 18, age = 3, diet = null, location = null, demeanor = "cuddly", kidFriendly = true, petFriendly = true, gender = null, adoptionStatus = "adopted", Employee_ID = null },
+            //    new Animal {ID = 1003, name = "Rowdy", breed = null, weight = 20, age = 8, diet = null, location = null, demeanor = "deceased", kidFriendly = true, petFriendly = true, gender = null, adoptionStatus = "not adopted", Employee_ID = null },
+            //};
+            //PointOfEntry.Run();
+            Import import = new Import();
+            import.ImportCSV();
         }
     }
 }
